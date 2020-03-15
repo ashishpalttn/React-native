5bf96de305c2839d94942eda6d583effb3ee2087
ashish pal <ashish@Ashishs-MacBook-Pro.local>
2020-03-15 17:25:29 +0530

    optimise app

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,38 +1,66 @@
 import React from 'react';
-import {StyleSheet, Text, SafeAreaView,Alert,BackHandler,TouchableOpacity } from 'react-native';
+import {StyleSheet, Text, SafeAreaView,Alert,BackHandler,TouchableOpacity,Clipboard } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 class alert extends React.Component {
   constructor() {
     super();
-  }
+    this.state={
+      count:0,
+      txt:'',
+      text:'',
+    }
+  };
+  readFromClipboard = async () => {
+    const clipboardContent = await Clipboard.getString(); 
+    this.setState({ clipboardContent }); 
+  };
+  writeToClipboard = async () => {
+      await Clipboard.setString(this.state.text);
+      alert('Copied to Clipboard!');
+  };
   abc=()=> {
     Alert.alert(
     'Alert Title',
     'My Alert Msg',
     [
-      {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
       {
         text: 'Cancel',
         onPress: () => console.log('Cancel Pressed'),
         style: 'cancel',
       },
-      {text: 'OK', onPress: () => console.log('OK Pressed')},
+      {text: 'OK', onPress: () => this.props.navigation.navigate('next')},
     ],
     {cancelable: false},
   );
   }
+
   render() {
 
     return (
       <SafeAreaView style={styles.container}>
+        <Text >{this.state.txt}</Text>
+        <TextInput style={styles.textInput} onChangeText={text=>this.setState({text})} value={this.state.text} placeholder='Tyle here....'></TextInput>
+        <TouchableOpacity onPress={this.writeToClipboard}><Text>Copy content</Text></TouchableOpacity>
         <TouchableOpacity onPress={this.abc}>
         <Text style={styles.text}>Home</Text>
+        <Text>{this.state.clipboardContent}</Text>
+        <TouchableOpacity onPress={this.readFromClipboard}><Text>Paste</Text></TouchableOpacity>
         </TouchableOpacity>
       </SafeAreaView>
     );
   }
   componentDidMount(){
     this.hello = BackHandler.addEventListener('hardwareBackPress', ()=> {
-      this.abc()
+      this.setState({txt:'Backhandler Triggered'})
+      this.setState({count:this.state.count+1})
+      setTimeout(()=>{
+        this.setState({txt:''})
+        this.setState({count:0})
+      },2000)
+      if(this.state.count==2){
+        this.abc()
+        this.setState({count:0})
+      }
       return true;
     });
   }
@@ -52,8 +80,17 @@ const styles = StyleSheet.create({
     width:100,
     height:50,
     backgroundColor:"white",
-    fontSize:35
+    fontSize:35,
+    margin:10
 
-  }
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '80%',
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
 });
 export default alert;
---
+++ src/components/main.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import {SafeAreaView,Text,StyleSheet, View,TouchableOpacity,ScrollView} from 'react-native'
+class Main extends React.Component{
+render(){
+    const {navigation}=this.props
+    return(
+        <SafeAreaView style={styles.container}>
+            <View style={styles.scroll_view}>
+                <Text style={{alignSelf:'center',fontSize:50,marginTop:30,justifyContent:"center",color:"blue"}}>Welcome</Text>
+                <ScrollView contentContainerStyle={{alignItems:"center"}}>
+                    <TouchableOpacity onPress={()=>{navigation.navigate('Home')}}>
+                    <View style={styles.button_view}><Text style={styles.button}>Home </Text></View>                    
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{navigation.navigate('tata_sky')}}>
+                    <View style={styles.button_view}><Text style={styles.button}>Tata Sky</Text></View>                    
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{navigation.navigate('image')}}>
+                    <View style={styles.button_view}><Text style={styles.button}>Touch Image</Text></View>                    
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{navigation.navigate('pass_value')}}>
+                    <View style={styles.button_view}><Text style={styles.button}>Profiles</Text></View>                    
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{navigation.navigate('section_list')}}>
+                    <View style={styles.button_view}><Text style={styles.button}>Section List</Text></View>                    
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{navigation.navigate('alert_box')}}>
+                    <View style={styles.button_view}><Text style={styles.button}>Alert</Text></View>                    
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{navigation.navigate('modal')}}>
+                    <View style={styles.button_view}><Text style={styles.button}>Modal</Text></View>                    
+                    </TouchableOpacity>
+                    <TouchableOpacity >
+                    <View style={styles.button_view}><Text style={styles.button}>Hello</Text></View>                    
+                    </TouchableOpacity>
+                    <TouchableOpacity >
+                    <View style={styles.button_view}><Text style={styles.button}>Hello</Text></View>                    
+                    </TouchableOpacity>
+                </ScrollView>
+            </View>
+        </SafeAreaView>
+    )
+}
+}
+const styles=StyleSheet.create({
+    container:{
+        flex :1,
+        backgroundColor:"blue",
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    scroll_view:{
+        flex:1,
+        backgroundColor:"white",
+        width:'85%',
+        borderRadius:30,
+        marginVertical:30,
+        paddingBottom:20,
+
+    },
+    button_view:{
+        borderRadius:20,
+        paddingHorizontal:15,
+        marginVertical:10,
+        backgroundColor:"blue"},
+    
+    button:{
+        // width:'80%'
+        backgroundColor:'blue',
+        // width:100,
+        fontSize:30,
+        color:'white',
+        paddingHorizontal:30,
+        borderRadius:50,
+        // marginTop:15,
+        alignSelf:'center'
+    }
+})
+export default Main;
